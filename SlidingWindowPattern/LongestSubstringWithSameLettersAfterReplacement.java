04e74b37e7db7c272a7637f81a469b8c4eb3a2cd
Prabir <lazydeveloperprabir@gmail.com>
2020-04-02 22:50:10 +0530

    unfished checkin

---
+++ SlidingWindowPattern/LongestSubstringWithSameLettersAfterReplacement.java
@@ -0,0 +1,15 @@
+package SlidingWindowPattern;
+
+/**
+ * LongestSubstringWithSameLettersAfterReplacement
+ */
+public class LongestSubstringWithSameLettersAfterReplacement {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static int getLength(String s, int k) {
+        
+    }
+}
