cd93b04a0378119f05ebb9b83e60ff6014f91d4a
Prabir <lazydeveloperprabir@gmail.com>
2020-04-07 20:53:12 +0530

    commit

--- ThirtyDaysChallenge/GroupAnagrams.java
+++ ThirtyDaysChallenge/GroupAnagrams.java
@@ -52,29 +52,29 @@ public class GroupAnagrams {
         return response;
     }
 
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,ArrayList>H = new HashMap<>();
-         for(int i=0;i<strs.length;i++){
-             char x[] = strs[i].toCharArray();
-             Arrays.sort(x);
-             String a = new String(x);
-             if(H.containsKey(a)){
-                 ArrayList<String>B = H.get(a);
-                 B.add(strs[i]);
-                 H.remove(a);
-                 H.put(a,B);
-             }
-             else{
-                 ArrayList<String>B = new ArrayList<>();
-                 B.add(strs[i]);
-                 H.put(a,B);
-             }
-         }
-         List<List<String>>A = new LinkedList<>();
-         for(String s: H.keySet()){
-             List<String>B = H.get(s);
-             A.add(B);
-         }
-         return A;
-     }
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     HashMap<String,ArrayList>H = new HashMap<>();
+    //      for(int i=0;i<strs.length;i++){
+    //          char x[] = strs[i].toCharArray();
+    //          Arrays.sort(x);
+    //          String a = new String(x);
+    //          if(H.containsKey(a)){
+    //              ArrayList<String>B = H.get(a);
+    //              B.add(strs[i]);
+    //              H.remove(a);
+    //              H.put(a,B);
+    //          }
+    //          else{
+    //              ArrayList<String>B = new ArrayList<>();
+    //              B.add(strs[i]);
+    //              H.put(a,B);
+    //          }
+    //      }
+    //      List<List<String>>A = new LinkedList<>();
+    //      for(String s: H.keySet()){
+    //          List<String>B = H.get(s);
+    //          A.add(B);
+    //      }
+    //      return A;
+    //  }
 }
